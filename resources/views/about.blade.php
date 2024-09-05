f9ed6fab29532ce3744928fe5b7bb3ba764405ee
Samuel Josefano Kaloh <kalohsamuel@gmail.com>
2024-09-05 11:32:01 +0700

    Update Homepage
    New components for homepage.

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>About Us</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1>About Us!</h1>
+    <a href="/" class="button bg-red-600">Homepage</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
+});
+
+Route::get('/about', function () {
+    return view('about');
 });
